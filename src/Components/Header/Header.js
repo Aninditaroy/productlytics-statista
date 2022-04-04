7ab3926480227ddb4ee4ea2aa3f33e852fc3ff07
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-04 23:47:36 +0600

    active link added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
         <div className='nav-div pt-4 pb-4'>
             <nav>
                 <div className='nav-links'>
-                    <NavLink className="link" to="/">HOME</NavLink>
-                    <NavLink className="link" to="/reviews">REVIEWS</NavLink>
-                    <NavLink className="link" to="/dashboard">DASHBOARD</NavLink>
-                    <NavLink className="link" to="/blogs">BLOGS</NavLink>
-                    <NavLink className="link" to="/about">ABOUT</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/">HOME</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")}  to="/reviews">REVIEWS</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")}  to="/dashboard">DASHBOARD</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")}  to="/blogs">BLOGS</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")}  to="/about">ABOUT</NavLink>
                 </div>
             </nav>
         </div>
