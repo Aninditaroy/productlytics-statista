af568e9e71564ac62509dedd542fa43f3d663566
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-04 12:17:16 +0600

    home section modified

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,20 +4,24 @@ import Product from "../../Assests/Images/product.png";
 import './Home.css';
 const Home = () => {
     return (
-        <div>
-            <Container fluid className='home-section d-flex justify-content-center'>
-                <Row className='details-section' >
-                    <Col sm={12} md={6} className="mt-5">
+        <>
+            <Container fluid className='home-section'>
+                <Row className='details-section'>
+                    <Col sm={12} md={7} className="mt-5">
                         <h1 >Say Hello to Your next iMac</h1>
                         <h1 className='heading-color'>Fit perfectly into your life.</h1>
-                        <p>iMac was designed to minimize its impact on the environment. All of the virgin wood fiber in its packaging comes exclusively from responsibly managed forests. The rare earth elements in the speakers, fan motor, and power connector magnets are completely recycled. And we're committed to transitioning our entire manufacturing supply chain to 100 percent renewable electricity by 2030.</p>
-                    </Col> 
-                    <Col sm={12} md={4}>
+                        <div className='ps-5 pe-5 m-3'>
+                            <p>iMac was designed to minimize its impact on the environment. All of the virgin wood fiber in its packaging comes exclusively from responsibly managed forests. The rare earth elements in the speakers, fan motor, and power connector magnets are completely recycled. And we're committed to transitioning our entire manufacturing supply chain to 100 percent renewable electricity by 2030.</p>
+                        </div>
+                        <button className='btn know-more-btn'><a target='_blank ' href="https://www.apple.com/imac-24/">Know More</a></button>
+                    </Col>
+                    <Col sm={12} md={5}>
                         <img src={Product} alt="" className='product-img' />
                     </Col>
                 </Row>
             </Container>
-        </div>
+           
+        </>
     );
 };
 
